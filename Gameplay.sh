073db58236a8cc96b70dc1b1fbb133657d7bf220
Mbee0 <143152376+Mbee0@users.noreply.github.com>
2023-12-24 14:31:21 -0800

    Create Gameplay.sh

---
+++ Gameplay.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Messaging the ESP 32"
+mosquitto_pub -h 34.28.215.180 -t transferCoords -m "ready"
+echo "Finished the message and beginning the loop..."
+
+while :
+do
+        receivedMessage=$(mosquitto_sub -h 34.28.215.180 -t transferSpots -C 1)
+        player=${receivedMessage:0:1}
+        available=${receivedMessage:1:1}
+        echo 'message received: '"$receivedMessage"
+        if [ "$player" == "X" ]; then
+                spotPicked=$(./playerOne.sh $available)
+                echo 'Spot x picked: '"$spotPicked"
+                mosquitto_pub -h 34.28.215.180 -t transferCoords -m 'X'"$spotPicked"
+        elif [ "$player" == "O" ]; then
+                spotPicked=$(./playerTwo.sh $available)
+                echo 'Spot o picked: '"$spotPicked"
+                mosquitto_pub -h 34.28.215.180 -t transferCoords -m 'O'"$spotPicked"
+        elif [ "$player" == "Q" ]; then
+                break
+        else
+                echo "got in"
+                echo "$receivedMessage" >> resultsOfTTT.txt
+                echo 'Sent message: '"$receivedMessage"
+        fi
+done
+
+echo "Game finished!" >> resultsOfTTT.txt
+
+echo "job is done :)"
